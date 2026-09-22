0575d53b2c2a8c5b538f4ef519808948d763dc1c
fuku-owl <t.sayasaya.t1212@gmail.com>
2026-09-22 11:56:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE="pocoa-v5";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
+const CACHE="pocoa-v6";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="pocoa-v6").map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
